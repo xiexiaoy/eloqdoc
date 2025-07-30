b31f9bd6db2af8e6a405515721d92321123fbf3a
Jerry Zhao <ch3nzhao@gmail.com>
2025-07-30 16:31:07 +0800

    fix pr ci script (#39)

--- concourse/scripts/pr.ent.bash
+++ concourse/scripts/pr.ent.bash
@@ -31,6 +31,14 @@ git submodule update --init --recursive
 cd src/mongo/db/modules/eloq
 ln -s $WORKSPACE/eloq_logservice_src eloq_log_service
 
+pushd eloq_log_service
+if [ -n "$pr_branch_name" ] && git ls-remote --exit-code --heads origin "$pr_branch_name" > /dev/null; then
+  git fetch origin '+refs/heads/*:refs/remotes/origin/*'
+  git checkout -b ${pr_branch_name} origin/${pr_branch_name}
+  git submodule update --init --recursive
+fi
+popd
+
 pushd tx_service
 ln -s $WORKSPACE/raft_host_manager_src raft_host_manager
 popd
